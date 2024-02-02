a476ab78259c27e914e7c313039cafb1871ab3ad
manuelyanez.ar <manuelyanez.ar@gmail.com>
2024-02-02 17:05:29 -0300

    About view added

--- portafolio_github_pages/urls.py
+++ portafolio_github_pages/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import hello
+from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello),
+    path('', views.hello),
+    path('about/', views.about),
 ]
